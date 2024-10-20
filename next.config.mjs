98248c66ee64f460e7e1a6e1d47f5bcb7a47e1a6
juanbaquiroga <juanbaquiroga@gmail.com>
2024-10-20 19:29:58 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'i.postimg.cc',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    domains: ['i.postimg.cc']
   }
 }
   
