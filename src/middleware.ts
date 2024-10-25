4b7deb5b6c15593cda8113fe80500db28ec11f18
juanbaquiroga <juanbaquiroga@gmail.com>
2024-10-25 12:51:10 -0300

    fix: middleware error

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,16 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(request: NextRequest) {
   const userAgent = request.headers.get('user-agent');
 
-  // const isMobile = () => {
-  //   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-  // };
   if (userAgent) {
     // Lógica para procesar el user agent
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
+    const isMobile = () => {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+    };
+    
+    if (isMobile()) {
       return NextResponse.redirect(new URL('/mobile', request.url));
     }
   }
-
-  // if (isMobile()) {
-  //   return NextResponse.redirect(new URL('/mobile', request.url));
-  // }
 
   return NextResponse.next();
 }
