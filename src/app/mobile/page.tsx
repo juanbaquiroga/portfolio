d667a815fb358647f8f9197e52b94f9f95166096
juanbaquiroga <juanbaquiroga@gmail.com>
2024-11-04 23:34:37 -0300

    feat: mobile route added

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -1,18 +1,11 @@
-import { Main } from "@/layouts/Main"
-import { About_me } from "@/layouts/About_me";
-import Sidebar from "@/layouts/Sidebar";
 import { Background } from "@/components/Background";
-import { Projects } from "@/layouts/Projects";
-import { Technologies } from "@/layouts/Technologies";
-import { Contact } from "@/layouts/Contact";
-import { Footer } from "@/layouts/Footer";
-import { Error } from "@/layouts/Error"
+import { Main } from "@/mobile/Main";
+
 
 export default function Home() {
   return (
     <>
-    {/* <Sidebar/>*/}
-    <Error/>
+    <Main/>
     <Background/> 
     </>
   );
