a899cb4a9b52e8a1098158932c60828c3b41ec67
juanbaquiroga <juanbaquiroga@gmail.com>
2024-11-06 17:43:19 -0300

    feat(mobile): footer section added

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -1,6 +1,7 @@
 import { Background } from "@/components/Background";
 import { About_me } from "@/mobile/About_me";
 import { Contact } from "@/mobile/Contact";
+import { Footer } from "@/mobile/Footer";
 import { Main } from "@/mobile/Main";
 import { Projects } from "@/mobile/Projects";
 import { Technologies } from "@/mobile/Technologies";
@@ -15,6 +16,7 @@ export default function Home() {
     <Projects/>
     <Technologies/>
     <Contact/>
+    <Footer/>
     </>
   );
 }
