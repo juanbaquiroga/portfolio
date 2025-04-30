d6ad17c6237154d811d8cd72277b29836c8b5552
juanbaquiroga <juanbaquiroga@gmail.com>
2025-04-30 16:43:03 -0300

    fix: sidebar hover

--- src/layouts/Sidebar/index.tsx
+++ src/layouts/Sidebar/index.tsx
@@ -27,7 +27,6 @@ const Sidebar = () => {
             <motion.div
                 className={styles.activeZone}
                 onMouseEnter={() => setIsVisible(true)}
-                onMouseLeave={() => setIsVisible(false)}
                 transition={{ type: "spring", stiffness: 200, damping: 15 }}
                 style={
                     isVisible
@@ -40,7 +39,6 @@ const Sidebar = () => {
             ></motion.div>
             <motion.div
                 className={styles.sidebar}
-                onMouseEnter={() => setIsVisible(true)}
                 onMouseLeave={() => setIsVisible(false)}
                 animate={{
                     y: "-50%",
@@ -50,16 +48,16 @@ const Sidebar = () => {
             >
                 {/* Logo */}
                 <div className={styles.logoContainer}>
-                    <motion.svg className={styles.svg} viewBox="0 0 204 152">
-                        <motion.path
+                    <svg className={styles.svg} viewBox="0 0 204 152">
+                        <path
                             className={styles.pathFill}
                             d="M54.5 0.500046V94C54.5 106.167 45.8 126.2 11 109L1.5 142C15.3333 149.667 50.5 159.7 76.5 138.5V148.5H151.5C215.5 148.5 215.5 84.5 174.5 73C205.5 57 205 8.00005 163 0.500046H54.5ZM117.5 115V90H153.5C162.5 95.5 162.5 108.5 153.5 115H117.5ZM148 58.0001H117.5V34.5H148C158.5 41.5 158.5 52.5001 148 58.0001Z"
                         />
-                        <motion.path
+                        <path
                             className={styles.path}
                             d="M76.5 148.5V138.5C50.5 159.7 15.3333 149.667 1.5 142L11 109C45.8 126.2 54.5 106.167 54.5 94V0.500046H163C205 8.00005 205.5 57 174.5 73C215.5 84.5 215.5 148.5 151.5 148.5M76.5 148.5C100.667 148.5 149.5 148.5 151.5 148.5M76.5 148.5H151.5M117.5 90V115H153.5C162.5 108.5 162.5 95.5 153.5 90H117.5ZM117.5 58.0001H148C158.5 52.5001 158.5 41.5 148 34.5H117.5V58.0001Z"
                         />
-                    </motion.svg>
+                    </svg>
                 </div>
 
                 {/* Navigation Menu */}
@@ -76,12 +74,12 @@ const Sidebar = () => {
                                     isActive
                                         ? {
                                               fontSize: "20px",
-                                              width: "150px",
+                                              width: "150%",
                                               height: "75px",
                                           }
                                         : {
                                               fontSize: "10px",
-                                              width: "90px",
+                                              width: "100%",
                                               height: "30px",
                                           }
                                 }
