cc13b10598a71e16b41bc760d218e5a78a39fca1
juanbaquiroga <juanbaquiroga@gmail.com>
2025-05-21 12:32:38 -0300

    perf: server side render

--- src/components/Slider/Card/index.tsx
+++ src/components/Slider/Card/index.tsx
@@ -1,55 +1,15 @@
-"use client";
-import React, { useRef } from "react";
-import gsap from "gsap";
 import styles from "./Card.module.scss";
 import { Itechnology } from "@/interfaces/technology.interface";
 
 export const Card = ({ id, title, d, viewbox }: Itechnology) => {
-    const svgWrapperRef = useRef<HTMLDivElement>(null);
-    const titleWrapperRef = useRef<HTMLDivElement>(null);
-
-    const handleMouseEnter = () => {
-        gsap.to(svgWrapperRef.current, {
-            opacity: 0,
-            scale: 1.1,
-            duration: 0.35,
-            ease: "power2.out"
-        });
-        gsap.to(titleWrapperRef.current, {
-            opacity: 1,
-            scale: 1,
-            duration: 0.35,
-            ease: "power2.out"
-        });
-    };
-
-    const handleMouseLeave = () => {
-        gsap.to(svgWrapperRef.current, {
-            opacity: 1,
-            scale: 1,
-            duration: 0.35,
-            ease: "power2.out"
-        });
-        gsap.to(titleWrapperRef.current, {
-            opacity: 0,
-            scale: 0.8,
-            duration: 0.35,
-            ease: "power2.out"
-        });
-    };
-
     return (
-        <div
-            className={styles.card}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-        >
-            <div className={styles.svgWrapper} ref={svgWrapperRef}>
+        <div className={styles.card}>
+            <div className={styles.svgWrapper}>
                 <svg className={styles.svg} viewBox={viewbox}>
                     <path className={styles.path} d={d}></path>
                 </svg>
             </div>
-            <div className={styles.titleWrapper} ref={titleWrapperRef}>
+            <div className={styles.titleWrapper}>
                 <span className={styles.title}>{title}</span>
             </div>
         </div>
